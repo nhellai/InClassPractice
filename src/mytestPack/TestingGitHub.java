77f4eb6ad25dcd803799cc163aff463ea3b3ec75
hellai <hnoorzai@ymail.com>
2022-11-29 11:39:20 -0500

    this is 2nd commit

--- src/mytestPack/TestingGitHub.java
+++ src/mytestPack/TestingGitHub.java
@@ -6,5 +6,14 @@ public class TestingGitHub {
 		// TODO Auto-generated method stub
 		System.out.println("Github practice section");
 	}
+		
+		public void iKnowGitHub() {
+			System.out.println("This is a test stage for the github practice session.");
+		}
+		
+		public void javaIsAlwaysFun() {
+			System.out.println("I am very confident in GitHub now.");
+		}
+	
 
 }
