b774f2f54aee49290cc17b51e3e28dc0ed1b5392
hellai <hnoorzai@ymail.com>
2022-11-29 11:24:01 -0500

    branching

---
+++ src/mytestPack/TestingGitHub.java
@@ -0,0 +1,10 @@
+package mytestPack;
+
+public class TestingGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Github practice section");
+	}
+
+}
